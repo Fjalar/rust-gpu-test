55a094b25eab205884f5ad8690bc422fcb7d21c1
Fjalar <fjalarhognason@gmail.com>
2026-05-11 10:20:04 +0200

    Triangle ppm output

--- shader/src/lib.rs
+++ shader/src/lib.rs
@@ -1,6 +1,6 @@
 #![cfg_attr(target_arch = "spirv", no_std)]
 
-use spirv_std::glam::UVec3;
+use spirv_std::glam::{UVec3, Vec4, vec4};
 use spirv_std::spirv;
 
 #[spirv(compute(threads(64)))]
@@ -13,4 +13,23 @@ pub fn main_cs(
         let v = data[i];
         data[i] = v * v;
     }
+}
+
+// Verbatim from rust-gpu/examples/shaders/simplest-shader.
+#[spirv(fragment)]
+pub fn main_fs(output: &mut Vec4) {
+    *output = vec4(1.0, 0.0, 0.0, 1.0);
+}
+
+#[spirv(vertex)]
+pub fn main_vs(
+    #[spirv(vertex_index)] vert_id: i32,
+    #[spirv(position, invariant)] out_pos: &mut Vec4,
+) {
+    *out_pos = vec4(
+        (vert_id - 1) as f32,
+        ((vert_id & 1) * 2 - 1) as f32,
+        0.0,
+        1.0,
+    );
 }
